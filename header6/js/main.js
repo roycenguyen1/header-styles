69e83025287ee6b59ba65a709b3c6fa990f8efaf
Royce <royce_nguyen@yahoo.com>
2020-11-10 23:03:12 -0500

    update

---
+++ header6/js/main.js
@@ -0,0 +1,27 @@
+$(".nav-link").click(function () {
+  if ($(this).parent().is(".has-sub")) {
+    $(this).toggleClass("show");
+  } else {
+    $(this).addClass("show");
+  }
+
+  $(this).parent().siblings().children().removeClass("show");
+  if ($(this).parent().siblings().is(".has-sub")) {
+    $(".submenu").slideUp("fast");
+  }
+});
+
+$(".has-sub").click(function () {
+  // $(".submenu").toggleClass("active");
+  $(".submenu").slideToggle("fast");
+  $(".submenu-item").click(function () {
+    if ($(".nav-link").hasClass("show")) {
+      $(".nav-link").removeClass("show");
+    }
+  });
+});
+
+$(".hamburger").click(function () {
+  $(".header").toggleClass("active");
+  $(this).toggleClass("active");
+});
